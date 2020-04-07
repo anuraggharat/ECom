5ae9def8dd8ca1465cd7564d2f2acf25ac08508b
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-04-07 16:00:24 +0530

    "initial commit"

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,6 +1,40 @@
 const User = require("../models/user");
+const {check,validationResult} = require('express-validator')
 
-exports.signup = (req, res) => {
+
+//signin controller
+exports.signin = (req,res)=>{
+  const {email,password}= req.body;
+  if (!errors.isEmpty()){
+    return res.status(422).json({
+      error:errors.array()[0].msg
+    })
+  }
+  User.findOne({email},(err,user)=>{
+    if(err){
+      res.status(400).json({
+        error:"User email doesnot exist"
+      })
+    }
+    if(!user.authenticate(password)){
+        return res.status(401).json({
+          error:"Email and password does not match"
+        })
+    }
+  })
+
+}
+
+
+
+exports.signup = (req, res) => { 
+  const errors = validationResult(req)
+  if (!errors.isEmpty()){
+    return res.status(422).json({
+      error:errors.array()[0].msg
+    })
+  }
+  
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
@@ -16,6 +50,10 @@ exports.signup = (req, res) => {
   });
 };
 
+
+
+
+//signout controller
 exports.signout = (req, res) => {
   res.json({
     message: "User signout"
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,10 +1,19 @@
 //all auth routes here
 var express = require('express')
 var router = express.Router()
+const {check} = require('express-validator')
+const {signout,signup,signin}=require('../controllers/auth')
 
-const {signout,signup}=require('../controllers/auth')
+router.post("/signup",[
+    check("name","name should be atleast 3 character").isLength({min:3}),
+    check("email","Email is necessary").isEmail(),
+    check("password","password should be 8 characters long").isLength({min:8}),
+],signup)
 
-router.post("/signup",signup)
+router.post("/signin",[
+    check("email","Email is necessary").isEmail(),
+    check("password","password field is compulsary").isLength({min:8}),
+],signin)
 
 router.get("/signout",signout)
 
