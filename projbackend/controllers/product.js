3bcb414450dc3c9706632a8039798f7a5eedf02c
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-04-09 16:31:00 +0530

    "compressed images from website"

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -77,3 +77,70 @@ exports.photo=(req,res,next)=>{
     }
     next()
 }
+
+
+exports.deleteProduct=(req,res)=>{
+    let product = req.product
+    product.remove((err,deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                error:"Failed to delete the product"
+            })
+        }
+        res.json({
+            message:"deletion was a success",deletedProduct
+        })
+    })
+}
+
+exports.updateProduct=(req,res)=>{
+    let form = new formidable.IncomingForm()
+    form.keepExtensions=true
+    form.parse(req,(err,fields,file)=>{
+        if(err){
+            return res.status(400).json({
+                error:"problem with the image"
+            })
+        }
+
+        let product = req.product;
+        product=_.extend(product,fields)
+        //handle file here
+        if (file.photo){
+            if(file.photo.size>3000000){
+                return res.status(400).json({
+                    error:"File size too big"
+                })
+            }
+            product.photo.data=fs.readFileSync(file.photo.path)
+            product.photo.contentType=file.photo.type    
+        }
+        product.save((err,product)=>{
+            if(err){
+                es.status(400).json({
+                    error:"Updation product failed"
+                })
+            }
+            res.json(product)
+        })
+
+
+    })
+}
+exports.getAllProducts=(req,res)=>{
+    let limit = req.query.limit ? parseInt(req.query.limit) : 8 
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    Product.find()
+    .select("-product")
+    .populate("category")
+    .sort([[sortBy,"asc"]])
+    .limit(limit)
+    .exec((err,products)=>{
+        if(err){
+            return res.status(400).json({
+                error:"No product found"
+            })
+        }
+        res.json(products)
+    })
+}
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const router = express.Router()
 
-const {getProductById,createProduct,getProduct,photo}=require("../controllers/product")
+const {getProductById,createProduct,getProduct,photo,deleteProduct,updateProduct,getAllProducts}=require("../controllers/product")
 const {isSignedIn,isAdmin,isAuthenticated}=require("../controllers/auth")
 const {getUserById}=require("../controllers/user")
 
@@ -9,13 +9,13 @@ const {getUserById}=require("../controllers/user")
 router.param("userId",getUserById)
 router.param("productId",getProductById)
 
-router.post("/product/create/:userId",isSignedIn,isAdmin,isAuthenticated,createProduct)
+router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct)
 router.get("/product/:productId",getProduct)
 router.get("/product/photo/:productId",photo)
 
+router.delete("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,deleteProduct)
+router.put("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,updateProduct)
 
-
-
-
+router.get("/products",getAllProducts)
 
 module.exports=router
